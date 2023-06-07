932ab2010025dc21399c0293ac5f7a35f99250eb
Joni92 <jonigonzalez2492@gmail.com>
2023-06-07 18:34:07 -0300

    First commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,17 @@
+
+const circleType = new CircleType(
+    document.getElementById("rotated")
+).radius(80);
+
+$(window).scroll(function() {
+    let offset = $(window).scrollTop();
+    offset = offset * 0.4;
+
+    $(".circular-text").css({
+        "-moz-transform": "rotate(" + offset + "deg)",
+        "-webkit-transform": "rotate(" + offset + "deg)",
+        "-o-transform": "rotate(" + offset + "deg)",
+        "-ms-transform": "rotate(" + offset + "deg)",
+        transform: "rotate(" + offset + "deg)"
+    });
+});
